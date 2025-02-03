9b922c5e573dc26ef733ed7a63c716d4bdc4e995
Anthonin <akadi@artfx.fr>
2025-02-03 13:24:55 +0100

    [Generate] Git

---
+++ Shape.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "code/Math/Bounds.h"
+#include "code/Math/Matrix.h"
+#include "code/Math/Quat.h"
+
+class Shape
+{
+public:
+	enum class ShapeType
+	{
+		SHAPE_SPHERE,
+	};
+	
+	virtual Mat3 InertiaTensor() const = 0;
+	
+	virtual ShapeType GetType() const = 0;
+	virtual Vec3 GetCenterOfMass() const { return centerOfMass; }
+
+	virtual Bounds GetBounds(const Vec3& pos, const Quat& orient) const = 0;
+	virtual Bounds GetBounds() const = 0;
+	
+protected:
+	Vec3 centerOfMass;
+};
+
+class ShapeSphere : public Shape
+{
+public:
+	ShapeSphere(float radiusP) : radius(radiusP)
+	{
+		centerOfMass.Zero();
+	}
+	
+	Mat3 InertiaTensor() const override;
+	
+	ShapeType GetType() const override { return ShapeType::SHAPE_SPHERE; }
+	
+	Bounds GetBounds(const Vec3& pos, const Quat& orient) const override;
+	Bounds GetBounds() const override;
+	
+
+	float radius;
+};
